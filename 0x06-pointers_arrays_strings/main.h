17fb0f83284f5853ed336a2a30ba716cc82978e3
Omarkamal222 <omarmkamal96@gmail.com>
2023-03-02 16:47:32 -0800

    omar

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+MAIN.H file
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -14,4 +16,6 @@ char *rot13(char *a);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
+#endif
 
